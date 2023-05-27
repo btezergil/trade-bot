55002501f42e064709c5d6144f402d83cf70974a
Berke Tezergil <btezergil@atlassian.com>
2023-05-27 11:49:14 +0300

    Implemented functions to get CSV data of stocks

--- src/clojure_scraps/datagetter.clj
+++ src/clojure_scraps/datagetter.clj
@@ -1,24 +1,65 @@
 (ns clojure-scraps.datagetter
-  (:require [clojure.tools.logging :as log]
+  (:require [clojure.java.io :as io]
+            [clojure.tools.logging :as log]
             [cheshire.core :as cheshire]
             [clj-http.client :as client]
             [environ.core :refer [env]]
-            [clojure-scraps.aws :as aws-helper]))
+            [clojure-scraps.aws :as aws-helper])
+  (:import (com.fasterxml.jackson.core JsonParseException)))
 
 (def team-query-params {:symbol "TEAM"
                         :interval "15min"
                         :exchange "NASDAQ"})
-(def quote-base-url "https://api.twelvedata.com/quote")
-(def time-series-base-url "https://api.twelvedata.com/time_series")
+(def quote-url "https://api.twelvedata.com/quote")
+(def time-series-url "https://api.twelvedata.com/time_series")
+(def stock-data-path "/Users/btezergil/phd/790/project/data/")
+
+(defn save-unprocessed-symbol
+  [symbol]
+  (with-open [f (io/writer (str stock-data-path "unreads.txt") :append true)]
+    (spit f (str symbol "\n"))))
+
+(defn get-csv-data
+  "Queries the API for data"
+  [symbol]
+
+  (let [response (client/get time-series-url {:query-params {"symbol"     symbol
+                                                             "interval"   (:interval team-query-params)
+                                                             "exchange"   (:exchange team-query-params)
+                                                             "outputsize" 5000
+                                                             "format"     "CSV"
+                                                             "delimiter"  ","
+                                                             "apikey"     (env :twelvedata-apikey)}})
+        body (:body response)]
+    (try
+      (cheshire/parse-string body true)
+      (save-unprocessed-symbol symbol)
+      (catch JsonParseException e body))))
+
+(defn generate-csv-files
+  [symbol]
+  (with-open [f (io/writer (str stock-data-path symbol))]
+    (spit f (get-csv-data symbol))))
+
+(defn get-stocks
+  "Gets all available stocks"
+  []
+
+  (let [response (client/get "https://api.twelvedata.com/stocks" {:query-params {"exchange" "NASDAQ"
+                                                                                 "apikey" (env :twelvedata-apikey)}})
+        body (:body response)
+        data (:data (cheshire/parse-string body true))
+        symbols (map :symbol data)]
+    (map generate-csv-files symbols)))
 
 (defn get-quote
   "Queries the API for data"
   []
 
-  (let [response (client/get quote-base-url {:query-params {"symbol" (:symbol team-query-params)
-                                                            "interval" (:interval team-query-params)
-                                                            "exchange" (:exchange team-query-params)
-                                                            "apikey" (env :twelvedata-apikey)}})
+  (let [response (client/get quote-url {:query-params {"symbol"   (:symbol team-query-params)
+                                                       "interval" (:interval team-query-params)
+                                                       "exchange" (:exchange team-query-params)
+                                                       "apikey"   (env :twelvedata-apikey)}})
         body (:body response)]
     (cheshire/parse-string body true)))
 
@@ -26,11 +67,11 @@
   "Queries the API for data"
   [size]
 
-  (let [response (client/get time-series-base-url {:query-params {"symbol" (:symbol team-query-params)
-                                                                  "interval" (:interval team-query-params)
-                                                                  "exchange" (:exchange team-query-params)
-                                                                  "outputsize" size
-                                                                  "apikey" (env :twelvedata-apikey)}})
+  (let [response (client/get time-series-url {:query-params {"symbol"     (:symbol team-query-params)
+                                                             "interval"   (:interval team-query-params)
+                                                             "exchange"   (:exchange team-query-params)
+                                                             "outputsize" size
+                                                             "apikey"     (env :twelvedata-apikey)}})
         body (cheshire/parse-string (:body response) true)
         values (:values body)]
     values))
