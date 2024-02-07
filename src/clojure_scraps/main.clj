b85b18a0e7638a1b0a5a36b97b3f087effd9909f
Berke Tezergil <btezergil@atlassian.com>
2024-02-07 18:45:22 +0300

    Changed prints to logs, commented out the test code statements

--- src/clojure_scraps/main.clj
+++ src/clojure_scraps/main.clj
@@ -1,10 +1,12 @@
 (ns clojure-scraps.main
-  (:require [clojure-scraps.genetic :as g]))
+  (:require [clojure-scraps.genetic :as g]
+            [clojure.tools.logging :as log]
+            [clojure.pprint :as pp]))
 
 (defn run-evolution
   "Initial runner function, calls the accessor function to start evolution."
   []
-  (g/start-evolution))
+  (log/info (g/start-evolution)))
 
 (run-evolution)
 #_*e
