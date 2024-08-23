88566030fe1093f965f78053c4075e069eefc121
Berke Tezergil <btezergil@gmail.com>
2024-08-23 22:39:37 +0300

    Added DynamoDB library, write operations for strategy datatype

--- src/clojure_scraps/aws.clj
+++ src/clojure_scraps/aws.clj
@@ -12,6 +12,8 @@
 
 (def lambda (aws/client (merge {:api :lambda} shared-client-params)))
 
+(def ddb (aws/client (merge {:api :dynamodb} shared-client-params)))
+
 (defn list-sns-actions
   []
   (aws/ops sns))
@@ -20,8 +22,24 @@
   []
   (aws/ops lambda))
 
+(defn list-dynamodb-actions
+  []
+  (aws/ops ddb))
+
 (defn send-sns
   "Sends a SNS notification"
   [message]
   (log/info "sending an SNS")
-  (aws/invoke sns {:op :Publish :request {:Message message :TopicArn "arn:aws:sns:eu-central-1:994976387571:Sns-deneme-topic"}}))
+  (aws/invoke sns {:op :Publish
+                   :request {:Message message
+                             :TopicArn "arn:aws:sns:eu-central-1:994976387571:Sns-deneme-topic"}}))
+
+(defn write-to-table
+  "Writes the given item to given DynamoDB table"
+  [table-name data]
+
+  (log/info (format "writing to table %s" table-name))
+  (aws/invoke ddb {:op :PutItem
+                   :request {:TableName table-name
+                             :Item data}})
+  (log/info "write is completed"))
