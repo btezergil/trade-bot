5141ff99d63f41e494141ef9f074b4af0eee66c3
Berke Tezergil <btezergil@gmail.com>
2024-08-23 22:37:19 +0300

    Initial commit, Telegram bot implementation

---
+++ project.clj
@@ -0,0 +1,22 @@
+(defproject clojure-scraps "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [telegrambot-lib "2.3.0"]
+                 [cheshire "5.10.1"]
+                 [environ "1.2.0"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [com.cognitect.aws/api "0.8.656"]
+                 [com.cognitect.aws/endpoints "1.1.12.437"]
+                 [com.cognitect.aws/sns "836.2.1323.0"]]
+  :plugins [[lein-environ "1.2.0"]]
+  :repl-options {:init-ns clojure-scraps.core}
+  :profiles {:dev [:project/dev :profiles/dev]
+             :test [:project/test :profiles/test]
+             ;; only edit :profiles/* in profiles.clj
+             :profiles/dev  {}
+             :profiles/test {}
+             :project/dev {}
+             :project/test {}})
