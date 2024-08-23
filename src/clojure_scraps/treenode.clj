2909d5bc1a23278d74abd13d77095d2173702fce
Berke Tezergil <btezergil@gmail.com>
2024-08-23 22:40:44 +0300

    Added mutation and crossover methods for tree nodes, added sequence generation method for genetic

--- src/clojure_scraps/treenode.clj
+++ src/clojure_scraps/treenode.clj
@@ -6,7 +6,7 @@
             [clojure-scraps.indicators.pinbar :as pinbar]))
 
 (def operators [:and :or])
-(def prune-height 3)
+(def prune-height 2)
 (def indicator-count 5)
 (def operands (cons :identity (range indicator-count)))
 
@@ -22,25 +22,25 @@
 
 ; treenode is a three element list representing a tree node and its children
 ; with this structure, we can get the left child with first, right child with last, and the node itself with second.
-(defn generate-node
-  [height-remaining]
-  (if (> height-remaining 0)
-    [(generate-node (dec height-remaining)) (generate-operator) (generate-node (dec height-remaining))]
-    [(generate-operand) (generate-operator) (generate-operand)]))
-
-(generate-node 3)
-
 (defn generate-tree
-  ([] (generate-node prune-height))
-  ([height] (generate-node height)))
+  "Generates a tree with given height, recursively until leaves are reached. No parameter method uses prune-height as default."
+  ([] (generate-tree prune-height))
+  ([height-remaining] (if (> height-remaining 0)
+                        [(generate-tree (dec height-remaining)) (generate-operator) (generate-tree (dec height-remaining))]
+                        [(generate-operand) (generate-operator) (generate-operand)])))
+
+; TODO: tree yapisi icin genel olarak bir spec yazilabilir, generative testing vs icin de iyi olur
 
 (defn mutation
+  "Genetic mutation operation, flips given operator or randomly replaces given operand."
   [node-type node]
   (condp = node-type
     :operator (if (= :and node) :or :and)
     :operand (generate-operand)))
 
-(defn swap-mutation 
+(defn swap-mutation
+  "Swap mutation operation is the simpler genetic mutation operator that can act on the tree.
+   Basically, one node is selected to undergo mutation, which changes its value to a new random one." 
   [node]
   (if (vector? node)
     (let [prob (rand)
@@ -52,7 +52,54 @@
             :else [left mid (swap-mutation right)]))
     (mutation :operand node)))
 
+(defn subtree-mutation
+  "Subtree mutation operation is the more complex genetic mutation operator for the node structure.
+   It selects a node randomly and completely changes the tree on that node. 
+   While swap mutation acts on a node only for operators, subtree mutation changes the whole subtree.
+   If a leaf node is reached, randomly replaces the operand."
+   [node height]
+   (if (vector? node) 
+     (let [propagation-probability (rand)
+           node-probability (rand)
+           left (first node)
+           mid (second node)
+           right (last node)]
+       (cond (< propagation-probability 0.5) (if (< node-probability 0.5)
+                                               [(subtree-mutation left (dec height)) mid right]
+                                               [left mid (subtree-mutation right (dec height))]) 
+             :else (if (< node-probability 0.5)
+                     [(generate-tree height) mid right]
+                     [left mid (generate-tree height)])))
+     (mutation :operand node)))
+
+(defn crossover
+  "Genetic crossover operator for the node structure, swaps two branches of different trees."
+  [node1 node2]
+  (if (vector? node1) 
+    (let [propagation-probability (rand)
+          node-probability (rand)
+          left1 (first node1)
+          mid1 (second node1)
+          right1 (last node1)
+          left2 (first node2)
+          mid2 (second node2)
+          right2 (last node2)]
+      (cond (< propagation-probability 0.5) (if (< node-probability 0.5)
+                                              (let [crossover-result (crossover left1 left2)
+                                                    new-left1 (first crossover-result)
+                                                    new-left2 (second crossover-result)]
+                                                [[new-left1 mid1 right1] [new-left2 mid2 right2]])
+                                              (let [crossover-result (crossover right1 right2)
+                                                    new-right1 (first crossover-result)
+                                                    new-right2 (second crossover-result)]
+                                                [[left1 mid1 new-right1] [left2 mid2 new-right2]])) 
+            :else (if (< node-probability 0.5)
+                    [[left2 mid1 right1] [left1 mid2 right2]]
+                    [[left1 mid1 right2] [left2 mid2 right1]])))
+    [node2 node1]))
+
 (defn check-signal-for-operand
+  "Signal check for operand, if a leaf node is found, checks whether the given signal map contains a signal that fits with the tree type at hand."
   [operand signals tree-type]
   (if (= tree-type ((-> operand
                         str
