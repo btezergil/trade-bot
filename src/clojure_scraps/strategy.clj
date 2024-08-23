100fdb63c1f7f61697423e7ba9ab3f2b3cf4bb8b
Berke Tezergil <btezergil@gmail.com>
2024-08-23 22:40:04 +0300

    Added bar calculation, indicator and strategy generation for RSI

--- src/clojure_scraps/strategy.clj
+++ src/clojure_scraps/strategy.clj
@@ -1,15 +1,48 @@
 (ns clojure-scraps.strategy
   (:require [clojure.tools.logging :as log]
             [clj-uuid :as uuid]
-            [clojure-scraps.aws :as aws-helper]))
+            [clojure-scraps.aws :as aws-helper]
+            [clojure-scraps.datagetter :as datagetter])
+  (:import (java.time Duration ZoneId ZonedDateTime)
+           (java.time.format DateTimeFormatter)
+           [org.ta4j.core BaseStrategy BaseBarSeries BaseBarSeriesBuilder]
+           [org.ta4j.core.criteria pnl.AverageProfitCriterion LinearTransactionCostCriterion MaximumDrawdownCriterion NumberOfBarsCriterion]
+           org.ta4j.core.indicators.helpers.ClosePriceIndicator
+           org.ta4j.core.indicators.RSIIndicator
+           [org.ta4j.core.rules CrossedDownIndicatorRule CrossedUpIndicatorRule WaitForRule]))
 
 (def table-name "strategy-v1")
 (def table-key "strategyId")
+(defn datetime-parser
+  "Parses given datetime string in format yyyy-MM-dd HH:mm:ss."
+  [dt]
+  (ZonedDateTime/parse dt (.withZone (DateTimeFormatter/ofPattern "yyyy-MM-dd HH:mm:ss") (ZoneId/of "UTC"))))
 
 (defn get-data
   "Gets data for one step for dummy strategy signal generation"
-  []
-  40)
+  [size]
+  (reverse (datagetter/get-time-series size)))
+
+(defn series
+  "Bars should be a sequence of maps containing :datetime/:open/:high/:low/:close/:volume"
+  ([] (series (get-data 100)))
+  ([bars] (let [s (.build (BaseBarSeriesBuilder.))]
+            (doseq [{:keys [datetime open high low close volume]} bars]
+              (.addBar s (datetime-parser datetime) open high low close volume))
+            s)))
+
+(defn rsi-indicator
+  "Returns an RSI indicator with given bars"
+  [bars period]
+  (RSIIndicator. (ClosePriceIndicator. bars) period))
+
+(defn rsi-strat
+  "Generates a strategy based on RSI indicator"
+  [series period oversold-thresh overbought-thresh]
+  (let [rsi (rsi-indicator series period)
+        entry (CrossedDownIndicatorRule. rsi oversold-thresh)
+        exit (CrossedUpIndicatorRule. rsi overbought-thresh)]
+    (BaseStrategy. entry exit)))
 
 (defprotocol Indicator
   (init-indicator [indicator])
