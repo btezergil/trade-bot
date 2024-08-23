5bfc3d44d28f19df0346f44581ae99e9fda6a485
Berke Tezergil <btezergil@gmail.com>
2024-08-23 22:41:46 +0300

    Updated formatting

--- src/clojure_scraps/strategy.clj
+++ src/clojure_scraps/strategy.clj
@@ -14,24 +14,11 @@
   (fn [class-key args]
     (let [kns (when-let [x (namespace class-key)] (str x "."))
           class-str (str pre-str kns (name class-key) post-str)]
-      (clojure.lang.Reflector/invokeConstructor (resolve (symbol class-str))
-                                                (to-array args)))))
-(defn ind
-  [class-key & args]
-  (let [ctor (constructor "org.ta4j.core.indicators." "Indicator")]
-    (ctor class-key args)))
-(defn candle-ind
-  [class-key & args]
-  (let [ctor (constructor "org.ta4j.core.indicators.candles." "Indicator")]
-    (ctor class-key args)))
-(defn rule
-  [class-key & args]
-  (let [ctor (constructor "org.ta4j.core.rules." "Rule")]
-    (ctor class-key args)))
-(defn crit
-  [class-key & args]
-  (let [ctor (constructor "org.ta4j.core.criteria." "Criterion")]
-    (ctor class-key args)))
+      (clojure.lang.Reflector/invokeConstructor (resolve (symbol class-str)) (to-array args)))))
+(defn ind [class-key & args] (let [ctor (constructor "org.ta4j.core.indicators." "Indicator")] (ctor class-key args)))
+(defn candle-ind [class-key & args] (let [ctor (constructor "org.ta4j.core.indicators.candles." "Indicator")] (ctor class-key args)))
+(defn rule [class-key & args] (let [ctor (constructor "org.ta4j.core.rules." "Rule")] (ctor class-key args)))
+(defn crit [class-key & args] (let [ctor (constructor "org.ta4j.core.criteria." "Criterion")] (ctor class-key args)))
 (defn base-strategy [entry-rule exit-rule] (BaseStrategy. entry-rule exit-rule))
 
 (defn get-profit
@@ -41,10 +28,7 @@
       .getProfit
       .doubleValue))
 
-(defn calculate-result
-  "Gets the positions from a trading record and calculates the total profit/loss"
-  [trading-record]
-  (reduce + (map get-profit trading-record)))
+(defn calculate-result "Gets the positions from a trading record and calculates the total profit/loss" [trading-record] (reduce + (map get-profit trading-record)))
 
 (defn get-indicator-value
   "Returns the indicator value on the given index."
@@ -61,8 +45,7 @@
           indicator-value-current (get-indicator-value indicator index)
           bar-close-before (datagetter/get-bar-value-at-index bars (dec index))
           bar-close-current (datagetter/get-bar-value-at-index bars index)]
-      (and (< indicator-value-before bar-close-before)
-           (> indicator-value-current bar-close-current)))
+      (and (< indicator-value-before bar-close-before) (> indicator-value-current bar-close-current)))
     false))
 
 (defn crosses-down?
@@ -73,8 +56,7 @@
           indicator-value-current (get-indicator-value indicator index)
           bar-close-before (datagetter/get-bar-value-at-index bars (dec index))
           bar-close-current (datagetter/get-bar-value-at-index bars index)]
-      (and (> indicator-value-before bar-close-before)
-           (< indicator-value-current bar-close-current)))
+      (and (> indicator-value-before bar-close-before) (< indicator-value-current bar-close-current)))
     false))
 
 (defn indicators-cross-up?
@@ -85,8 +67,7 @@
           ind1-value-current (get-indicator-value ind1 index)
           ind2-value-before (get-indicator-value ind2 (dec index))
           ind2-value-current (get-indicator-value ind2 index)]
-      (and (< ind1-value-before ind2-value-before)
-           (> ind1-value-current ind2-value-current)))
+      (and (< ind1-value-before ind2-value-before) (> ind1-value-current ind2-value-current)))
     false))
 
 (defn indicators-cross-down?
@@ -97,67 +78,40 @@
           ind1-value-current (get-indicator-value ind1 index)
           ind2-value-before (get-indicator-value ind2 (dec index))
           ind2-value-current (get-indicator-value ind2 index)]
-      (and (> ind1-value-before ind2-value-before)
-           (< ind1-value-current ind2-value-current)))
+      (and (> ind1-value-before ind2-value-before) (< ind1-value-current ind2-value-current)))
     false))
 
-(defn rsi-indicator
-  "Returns an RSI indicator with given bars"
-  [bars period]
-  (ind :RSI (ind :helpers/ClosePrice bars) period))
+(defn rsi-indicator "Returns an RSI indicator with given bars" [bars period] (ind :RSI (ind :helpers/ClosePrice bars) period))
 
-(defn sma-indicator
-  "Returns a SMA indicator with given bars"
-  [bars period]
-  (ind :SMA (ind :helpers/ClosePrice bars) period))
+(defn sma-indicator "Returns a SMA indicator with given bars" [bars period] (ind :SMA (ind :helpers/ClosePrice bars) period))
 
-(defn ema-indicator
-  "Returns a EMA indicator with given bars"
-  [bars period]
-  (ind :EMA (ind :helpers/ClosePrice bars) period))
+(defn ema-indicator "Returns a EMA indicator with given bars" [bars period] (ind :EMA (ind :helpers/ClosePrice bars) period))
 
-(defn engulfing-indicator
-  "Returns an engulfing indicator"
-  [bars]
-  (candle-ind :BullishEngulfing bars))
+(defn engulfing-indicator "Returns an engulfing indicator" [bars] (candle-ind :BullishEngulfing bars))
 
 (defn rsi-strategy
   "Generates a strategy based on RSI indicator"
   [series period oversold-thresh overbought-thresh]
-  (let [rsi (rsi-indicator series period)
-        entry (rule :CrossedDownIndicator rsi oversold-thresh)
-        exit (rule :CrossedUpIndicator rsi overbought-thresh)]
-    (base-strategy entry exit)))
+  (let [rsi (rsi-indicator series period) entry (rule :CrossedDownIndicator rsi oversold-thresh) exit (rule :CrossedUpIndicator rsi overbought-thresh)] (base-strategy entry exit)))
 
 (defn engulfing-strategy
   "Generates a strategy based on engulfing candlestick pattern"
   []
   (let [engulfing (engulfing-indicator (datagetter/get-bars))
         entry (rule :BooleanIndicator engulfing)
-        exit (rule :WaitFor Trade$TradeType/BUY 10)] ; exit rule nasil
-                                                     ; dusunuyoruz onu
-                                                     ; kararlastir
+        exit (rule :WaitFor Trade$TradeType/BUY 10)] ; exit rule nasil dusunuyoruz onu kararlastir
     (base-strategy entry exit)))
 
 (defn hammer-strategy
   []
   (let [ind (pinbar/create-hammer-indicator (datagetter/get-bars))
         entry (rule :BooleanIndicator ind)
-        exit (rule :WaitFor Trade$TradeType/BUY 10)] ; exit rule naisl
-                                                     ; dusunuyoruz onu
-                                                     ; kararlastir
+        exit (rule :WaitFor Trade$TradeType/BUY 10)] ; exit rule naisl dusunuyoruz onu kararlastir
     (base-strategy entry exit)))
 
-(defn run-strategy
-  "Runs the given strategy and returns the generated positions"
-  [strategy]
-  (let [bsm (BarSeriesManager. (datagetter/get-bars))]
-    (.getPositions (.run bsm strategy))))
+(defn run-strategy "Runs the given strategy and returns the generated positions" [strategy] (let [bsm (BarSeriesManager. (datagetter/get-bars))] (.getPositions (.run bsm strategy))))
 
-(defn run-rsi
-  [oversold overbought]
-  (let [strategy (rsi-strategy (datagetter/get-bars) 14 oversold overbought)]
-    (run-strategy strategy)))
+(defn run-rsi [oversold overbought] (let [strategy (rsi-strategy (datagetter/get-bars) 14 oversold overbought)] (run-strategy strategy)))
 
 (def run-engulfing (run-strategy (engulfing-strategy)))
 (def run-hammer (run-strategy (hammer-strategy)))
@@ -165,44 +119,32 @@
 (defn eng-criterion
   "Uses criterion to find profit BUT NOT MATCHING THE ACTUAL RESULT, DON'T USE!!!"
   [strategy]
-  (let [criterion (crit :pnl/NetProfit)
-        bars (datagetter/get-bars)
-        bsm (BarSeriesManager. bars)
-        rec (.run bsm strategy)]
-    (.calculate criterion bars rec)))
+  (let [criterion (crit :pnl/NetProfit) bars (datagetter/get-bars) bsm (BarSeriesManager. bars) rec (.run bsm strategy)] (.calculate criterion bars rec)))
 
 #_(calculate-result run-engulfing)
 #_(calculate-result run-hammer)
-; TODO: hammer buy rule ile alakali bir problem olabilir, acaba bu yuzden mi
-; hammer'da pozisyon acilmiyor?
+; TODO: hammer buy rule ile alakali bir problem olabilir, acaba bu yuzden mi hammer'da pozisyon acilmiyor?
 
 #_(map get-profit run-engulfing)
 #_(eng-criterion (engulfing-strategy))
-; TODO: calculate result ile net profit neden farkli donuyor? analiz etmek
-; lazim, criterion mu dogru benim fonksiyon mu?
+; TODO: calculate result ile net profit neden farkli donuyor? analiz etmek lazim, criterion mu dogru benim fonksiyon mu?
 
 #_(run-strategy (rsi-strategy))
 (calculate-result (run-rsi 30 70))
-; TODO: hammer ve shooting star icin candle indicator yaz, sonrasinda da bunlari
-; temel alan stratejiler olustur
+; TODO: hammer ve shooting star icin candle indicator yaz, sonrasinda da bunlari temel alan stratejiler olustur
 
 ; TODO: write-to-table yapisi nasil olacak, bunlara karar verip implement et"
 
 (defn write-to-table
   "Writes the given indicator map to the table"
   [indicator]
-  (let [entry {"strategyId" {:S (str (uuid/v1))},
-               "indicatorName" {:S "RSI"},
-               "buyThreshold" {:N (str (.buy-threshold indicator))},
-               "sellThreshold" {:N (str (.sell-threshold indicator))}}]
+  (let [entry {"strategyId" {:S (str (uuid/v1))}, "indicatorName" {:S "RSI"}, "buyThreshold" {:N (str (.buy-threshold indicator))}, "sellThreshold" {:N (str (.sell-threshold indicator))}}]
     (aws-helper/write-to-table (:table-name table-vars) entry)))
 
 (defn read-from-table
   "Reads the given id from strategy table and returns it as a map"
   [id]
-  (let [item (aws-helper/read-from-table (:table-name table-vars)
-                                         (:table-key table-vars)
-                                         id)
+  (let [item (aws-helper/read-from-table (:table-name table-vars) (:table-key table-vars) id)
         data-map (:Item item)
         indicator-name (-> data-map
                            :indicatorName
@@ -215,7 +157,4 @@
                            :sellThreshold
                            :N
                            parse-long)]
-    {:id id,
-     :type indicator-name,
-     :buy-threshold buy-threshold,
-     :sell-threshold sell-threshold}))
+    {:id id, :type indicator-name, :buy-threshold buy-threshold, :sell-threshold sell-threshold}))
--- src/clojure_scraps/treenode.clj
+++ src/clojure_scraps/treenode.clj
@@ -1,4 +1,4 @@
-(ns clojure-scraps.treenode 
+(ns clojure-scraps.treenode
   (:require [clojure.tools.logging :as log]
             [clojure-scraps.params :as p]
             [clojure.spec.alpha :as s]
@@ -12,106 +12,48 @@
 ; TODO: engulfing ve pinbar aslinda sinyal cikartacak seviyede hazir, ama nasil kullanacagimizdan emin olana kadar eklemeyelim
 (def operands [:identity :rsi :sma :ema :double-sma :double-ema])
 
-(defn generate-operator
-  "Generates a random operator, taken from the operators list."
-  []
-  (rand-nth operators))
+(defn generate-operator "Generates a random operator, taken from the operators list." [] (rand-nth operators))
 
 (defn rand-int-range
   "rand-int with minimum value, inclusive min, inclusive max"
   [min max]
   (-> max
-      inc 
+      inc
       (- min)
       rand-int
       (+ min)))
 
-(defn generate-rsi
-  [index]
-  {:index index
-   :indicator :rsi
-   :oversold (rand-int-range 15 45)
-   :overbought (rand-int-range 55 85)
-   :window (rand-int-range 8 20)})
+(defn generate-rsi [index] {:index index, :indicator :rsi, :oversold (rand-int-range 15 45), :overbought (rand-int-range 55 85), :window (rand-int-range 8 20)})
 
 (defn mutate-rsi
   [node]
-  (let [param-prob (rand)]
-    (condp < param-prob
-      0.33 (assoc node :oversold (rand-int-range 15 45)) 
-      0.66 (assoc node :overbought (rand-int-range 55 85))
-      (assoc node :window (rand-int-range 8 20)))))
+  (let [param-prob (rand)] (condp < param-prob 0.33 (assoc node :oversold (rand-int-range 15 45)) 0.66 (assoc node :overbought (rand-int-range 55 85)) (assoc node :window (rand-int-range 8 20)))))
 
-(defn generate-sma
-  [index]
-  {:index index
-   :indicator :sma
-   :window (rand-int-range 10 100)})
+(defn generate-sma [index] {:index index, :indicator :sma, :window (rand-int-range 10 100)})
 
-(defn generate-double-sma
-  [index]
-  {:index index
-   :indicator :double-sma
-   :window1 (rand-int-range 5 20)
-   :window2 (rand-int-range 40 80)})
+(defn generate-double-sma [index] {:index index, :indicator :double-sma, :window1 (rand-int-range 5 20), :window2 (rand-int-range 40 80)})
 
-(defn generate-ema
-  [index]
-  {:index index
-   :indicator :ema
-   :window (rand-int-range 10 100)})
+(defn generate-ema [index] {:index index, :indicator :ema, :window (rand-int-range 10 100)})
 
-(defn generate-double-ema
-  [index]
-  {:index index
-   :indicator :double-ema
-   :window1 (rand-int-range 5 20)
-   :window2 (rand-int-range 40 80)})
+(defn generate-double-ema [index] {:index index, :indicator :double-ema, :window1 (rand-int-range 5 20), :window2 (rand-int-range 40 80)})
 
-(defn mutate-ma
-  [node]
-  (assoc node :window (rand-int-range 10 100)))
+(defn mutate-ma [node] (assoc node :window (rand-int-range 10 100)))
 
-(defn mutate-double-ma
-  [node]
-  (let [param-prob (rand)]
-    (if (< param-prob 0.5)
-      (assoc node :window1 (rand-int-range 5 20))
-      (assoc node :window2 (rand-int-range 40 80)))))
+(defn mutate-double-ma [node] (let [param-prob (rand)] (if (< param-prob 0.5) (assoc node :window1 (rand-int-range 5 20)) (assoc node :window2 (rand-int-range 40 80)))))
 
-(defn generate-fisher
-  [index]
-  {:index index
-   :indicator :fisher
-   :window (rand-int-range 5 15)})
+(defn generate-fisher [index] {:index index, :indicator :fisher, :window (rand-int-range 5 15)})
 
-(defn mutate-fisher
-  [node]
-  node)
+(defn mutate-fisher [node] node)
 
-(defn generate-fibonacci
-  [index]
-  {:index index
-   :indicator :fibonacci})
+(defn generate-fibonacci [index] {:index index, :indicator :fibonacci})
 
-(defn mutate-fibonacci
-  [node]
-  node)
+(defn mutate-fibonacci [node] node)
 
-(defn generate-engulfing
-  [index]
-  {:index index
-   :indicator :engulfing})
+(defn generate-engulfing [index] {:index index, :indicator :engulfing})
 
-(defn generate-pinbar
-  [index]
-  {:index index
-   :indicator :pinbar})
+(defn generate-pinbar [index] {:index index, :indicator :pinbar})
 
-(defn generate-identity
-  [index]
-  {:index index
-   :indicator :identity})
+(defn generate-identity [index] {:index index, :indicator :identity})
 
 (defn generate-operand
   "Generates a random operand, taken from the operands list."
@@ -134,19 +76,23 @@
 (defn get-right-index-for-operand
   "Calculates the operand index for the right side of the tree."
   [height]
-  (-> 2 (Math/pow height) int))
+  (-> 2
+      (Math/pow height)
+      int))
 
 ; treenode is a three element list representing a tree node and its children
 ; with this structure, we can get the left child with first, right child with last, and the node itself with second.
 (defn generate-tree
   "Generates a tree with given height, recursively until leaves are reached. No parameter method uses prune-height as default. Also gives indices to operands."
   ([] (generate-tree (:prune-height p/params)))
-  ([height-remaining] (if (> height-remaining 0)
-                        [(generate-tree (dec height-remaining) 0) (generate-operator) (generate-tree (dec height-remaining) (get-right-index-for-operand (dec height-remaining)))]
-                        (generate-operand 0)))
-  ([height-remaining initial-index] (if (> height-remaining 0)
-                                      [(generate-tree (dec height-remaining) initial-index) (generate-operator) (generate-tree (dec height-remaining) (+ initial-index (get-right-index-for-operand (dec height-remaining))))]
-                                      (generate-operand initial-index))))
+  ([height-remaining]
+   (if (> height-remaining 0)
+     [(generate-tree (dec height-remaining) 0) (generate-operator) (generate-tree (dec height-remaining) (get-right-index-for-operand (dec height-remaining)))]
+     (generate-operand 0)))
+  ([height-remaining initial-index]
+   (if (> height-remaining 0)
+     [(generate-tree (dec height-remaining) initial-index) (generate-operator) (generate-tree (dec height-remaining) (+ initial-index (get-right-index-for-operand (dec height-remaining))))]
+     (generate-operand initial-index))))
 
 (defn perform-mutation
   "Genetic mutation operation, flips given operator or mutates given operand. Operand mutation can either be a change of parameter or replacement with a new one."
@@ -171,7 +117,7 @@
 
 (defn swap-mutation
   "Swap mutation operation is the simpler genetic mutation operator that can act on the tree.
-   Basically, one node is selected to undergo mutation, which changes its value to a new random one." 
+   Basically, one node is selected to undergo mutation, which changes its value to a new random one."
   [node]
   (log/debug "swap node: " node)
   (if (vector? node)
@@ -179,7 +125,7 @@
           left (first node)
           mid (second node)
           right (last node)]
-      (cond (< prob 0.2) [left (perform-mutation :operator mid) right] 
+      (cond (< prob 0.2) [left (perform-mutation :operator mid) right]
             (< prob 0.6) [(swap-mutation left) mid right]
             :else [left mid (swap-mutation right)]))
     (perform-mutation :operand node)))
@@ -187,38 +133,27 @@
 (defn find-initial-index
   "Returns the initial index of given tree, needed by subtree mutation since it can arbitrarily select any subtree to be replaced."
   [node]
-  (if (vector? node)
-    (find-initial-index (first node))
-    (:index node)))
+  (if (vector? node) (find-initial-index (first node)) (:index node)))
 
 (defn subtree-mutation
   "Subtree mutation operation is the more complex genetic mutation operator for the node structure.
    It selects a node randomly and completely changes the tree on that node. 
    While swap mutation acts on a node only for operators, subtree mutation changes the whole subtree.
    If a leaf node is reached, randomly replaces the operand."
-   ([node] (subtree-mutation node (:prune-height p/params)))
-   ([node height] 
-    (log/debug "subtree node: " node)
-    (if (vector? node) 
-      (let [propagation-probability (rand)
-            node-probability (rand)
-            left (first node)
-            mid (second node)
-            right (last node)]
-        (cond (< propagation-probability 0.5) (if (< node-probability 0.5)
-                                                [(subtree-mutation left (dec height)) mid right]
-                                                [left mid (subtree-mutation right (dec height))]) 
-              :else (if (< node-probability 0.5)
-                      [(generate-tree (dec height) (find-initial-index left)) mid right]
-                      [left mid (generate-tree (dec height) (find-initial-index right))])))
-      (perform-mutation :operand node))))
+  ([node] (subtree-mutation node (:prune-height p/params)))
+  ([node height]
+   (log/debug "subtree node: " node)
+   (if (vector? node)
+     (let [propagation-probability (rand)
+           node-probability (rand)
+           left (first node)
+           mid (second node)
+           right (last node)]
+       (cond (< propagation-probability 0.5) (if (< node-probability 0.5) [(subtree-mutation left (dec height)) mid right] [left mid (subtree-mutation right (dec height))])
+             :else (if (< node-probability 0.5) [(generate-tree (dec height) (find-initial-index left)) mid right] [left mid (generate-tree (dec height) (find-initial-index right))])))
+     (perform-mutation :operand node))))
 
-(defn mutate-tree
-  "Performs mutation on the given tree."
-  [node]
-  (if (< (rand) 0.5)
-       (swap-mutation node)
-       (subtree-mutation node)))
+(defn mutate-tree "Performs mutation on the given tree." [node] (if (< (rand) 0.5) (swap-mutation node) (subtree-mutation node)))
 
 (defn mutation
   "Genetic mutation operator for individials, performs either swap or subtree mutation on the given node defined in the individual.
@@ -229,19 +164,14 @@
         long-node (first seqn)
         short-node (second seqn)]
     (log/debug "mutation node: " seqn)
-    (io/build-individual (if (< (rand) 0.5) 
-                             (vector (mutate-tree long-node) short-node)
-                             (vector long-node (mutate-tree short-node)))
-                         (:parents ind)
-                         (:age ind)
-                         fitness-func)))
+    (io/build-individual (if (< (rand) 0.5) (vector (mutate-tree long-node) short-node) (vector long-node (mutate-tree short-node))) (:parents ind) (:age ind) fitness-func)))
 
 (defn node-crossover
   "Genetic crossover operator for the node structure, swaps two branches of different trees."
   [node1 node2]
   (log/debug "before node1: " node1)
   (log/debug "before node2: " node2)
-  (if (vector? node1) 
+  (if (vector? node1)
     (let [propagation-probability (rand)
           node-probability (rand)
           left1 (first node1)
@@ -250,48 +180,32 @@
           left2 (first node2)
           mid2 (second node2)
           right2 (last node2)]
-      (cond (< propagation-probability 0.5) (if (< node-probability 0.5)
-                                              (let [crossover-result (node-crossover left1 left2)
-                                                    new-left1 (first crossover-result)
-                                                    new-left2 (second crossover-result)]
-                                                [[new-left1 mid1 right1] [new-left2 mid2 right2]])
-                                              (let [crossover-result (node-crossover right1 right2)
-                                                    new-right1 (first crossover-result)
-                                                    new-right2 (second crossover-result)]
-                                                [[left1 mid1 new-right1] [left2 mid2 new-right2]])) 
-            :else (if (< node-probability 0.5)
-                    [[left2 mid1 right1] [left1 mid2 right2]]
-                    [[left1 mid1 right2] [left2 mid2 right1]])))
+      (cond (< propagation-probability 0.5)
+            (if (< node-probability 0.5)
+              (let [crossover-result (node-crossover left1 left2) new-left1 (first crossover-result) new-left2 (second crossover-result)] [[new-left1 mid1 right1] [new-left2 mid2 right2]])
+              (let [crossover-result (node-crossover right1 right2) new-right1 (first crossover-result) new-right2 (second crossover-result)] [[left1 mid1 new-right1] [left2 mid2 new-right2]]))
+            :else (if (< node-probability 0.5) [[left2 mid1 right1] [left1 mid2 right2]] [[left1 mid1 right2] [left2 mid2 right1]])))
     [node2 node1]))
 
-(defn tree-selector
-  "Helper function for crossover to return either the long tree selector (i.e. 'first' function) or the short one."
-  [long?]
-  (if long? first second))
+(defn tree-selector "Helper function for crossover to return either the long tree selector (i.e. 'first' function) or the short one." [long?] (if long? first second))
 
 (defn crossover
   "Genetic crossover operator for individuals, calls the crossover for nodes defined by the genetic sequence within individuals.
   Only performs crossover on the long or the short tree."
   ([fitness-func ind-list] (crossover fitness-func (first ind-list) (second ind-list)))
-  ([fitness-func ind1 ind2] 
+  ([fitness-func ind1 ind2]
    (let [seqn1 (:genetic-sequence ind1)
          seqn2 (:genetic-sequence ind2)
          long? (< (rand) 0.5)
          node1 ((tree-selector long?) seqn1)
          node2 ((tree-selector long?) seqn2)
-         crossover-result (node-crossover node1 node2)] 
-     (vector (io/build-individual (if long? 
-                                    (vector (first crossover-result) (second seqn1))
-                                    (vector (first seqn1) (first crossover-result)))
-                                  (vector seqn1 seqn2) ; TODO: parent'i dogru mu setliyoruz bi bakalim
-                                  (:default-age p/params)
-                                  fitness-func)
-             (io/build-individual (if long? 
-                                    (vector (second crossover-result) (second seqn2))
-                                    (vector (first seqn2) (second crossover-result)))
-                                  (vector seqn1 seqn2)
-                                  (:default-age p/params)
-                                  fitness-func)))))
+         crossover-result (node-crossover node1 node2)]
+     (vector
+      (io/build-individual (if long? (vector (first crossover-result) (second seqn1)) (vector (first seqn1) (first crossover-result)))
+                           (vector seqn1 seqn2) ; TODO: parent'i dogru mu setliyoruz bi bakalim
+                           (:default-age p/params)
+                           fitness-func)
+      (io/build-individual (if long? (vector (second crossover-result) (second seqn2)) (vector (first seqn2) (second crossover-result))) (vector seqn1 seqn2) (:default-age p/params) fitness-func)))))
 
 (defn index-to-keyword
   [operand]
@@ -304,9 +218,10 @@
   "Performs the signal check for operand, if a leaf node is found, checks whether the given signal map contains a signal that fits with the tree type at hand.
   Note that this method only checks whether the signal for this operand is fired for the tree type or not, it does not actually generate the signals. "
   [operand signals tree-type]
-  (if (= tree-type (-> operand
-                       index-to-keyword
-                       signals))
+  (if (= tree-type
+         (-> operand
+             index-to-keyword
+             signals))
     tree-type
     :no-signal))
 
@@ -320,15 +235,10 @@
           operator (second node)]
       (cond (= :identity left-signal) right-signal
             (= :identity right-signal) left-signal
-            (= :and operator) (if (= left-signal right-signal tree-type)
-                                tree-type
-                                :no-signal)
+            (= :and operator) (if (= left-signal right-signal tree-type) tree-type :no-signal)
             (= :or operator) (cond (= left-signal right-signal tree-type) tree-type
                                    (= :no-signal left-signal) right-signal
                                    (= :no-signal right-signal) left-signal
                                    :else :no-signal)))
-    (if (= :identity node) 
-      :identity
-      (check-signal-for-operand node signals tree-type))))
-
+    (if (= :identity node) :identity (check-signal-for-operand node signals tree-type))))
 
