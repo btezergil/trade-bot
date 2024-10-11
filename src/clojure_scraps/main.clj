94aa7fe9cb0ffdc1797cc8c3cfe92d9b61df67dd
Berke Tezergil <btezergil@gmail.com>
2024-10-11 15:14:48 +0300

    Added telemere library for logging, main function for running from terminal

--- src/clojure_scraps/main.clj
+++ src/clojure_scraps/main.clj
@@ -2,25 +2,43 @@
   (:require [clojure-scraps.genetic :as g]
             [clojure.tools.logging :as log]
             [clojure.pprint :as pp]
-            [clojure-scraps.bot :as tb]))
+            [clojure-scraps.bot :as tb]
+            [taoensso.telemere :as t]
+            [taoensso.telemere.tools-logging :as tl]))
 
 (defn run-evolution
   "Initial runner function, calls the accessor function to start evolution."
   []
   (let [out (java.io.StringWriter.)
         evolution-result (g/start-evolution)]
-    (pp/pprint (map (fn [res] (dissoc res :parents)) evolution-result) out)
+    (log/info (pp/pprint (map (fn [res] (dissoc res :parents)) evolution-result) out))
     (tb/message-to-me (.toString out))))
-
-(time (run-evolution))
-;*e
 
 (defn test-individual
   "Generates an individual and calculates its fitness for test purposes."
   []
   (let [ind (g/generate-sequence)
         data g/get-bar-series-for-experiments]
-    (println ind)
-    (g/calculate-fitness data ind)))
+    (log/info "Generated individual for test:" ind)
+    (log/info "Calculated fitness:" (g/calculate-fitness data ind))))
+
+(defn setup-telemere
+  "Telemere-related setup"
+  []
+  (tl/tools-logging->telemere!)
+  (t/streams->telemere!))
+
+(defn main
+  "Parses command line arguments and calls the related functions"
+  []
+  (setup-telemere)
+  (let [arg (first *command-line-args*)]
+    (condp = arg
+      "r" (run-evolution)
+      "t" (test-individual)
+      (log/warn "No cmdline args"))))
 
 ;(test-individual)
+;(run-evolution)
+;*e
+(main)
