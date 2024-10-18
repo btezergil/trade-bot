770e3c5c61a5164c7cb31ba30ed0c724ff514905
Berke Tezergil <btezergil@gmail.com>
2024-10-18 15:21:50 +0300

    Added methods to gather final statistics and write them to table

--- src/clojure_scraps/charts.clj
+++ src/clojure_scraps/charts.clj
@@ -1,21 +1,91 @@
 (ns clojure-scraps.charts
   (:require [clojure-scraps.dynamo :as dyn]
-            [oz.core :as oz]))
+            [oz.core :as oz]
+            [clojure.tools.logging :as log]))
 
 (oz/start-server!)
 
-(def data (flatten (map (fn [row] (list {:time (int (:generation-count row)) :item "average fitness" :quantity (double (:avg-fitness row))}
-                                        {:time (int (:generation-count row)) :item "best fitness" :quantity (double (:best-fitness row))}))
-                        (dyn/read-evolution-stats-from-table "4b8bda8a-9f08-495c-ac85-97bde86ab9ee"))))
+(defn get-evolution-stat-data
+  [evolution-id]
+  (flatten (map (fn [row] (list {:time (int (:generation-count row)) :item "average fitness" :quantity (double (:avg-fitness row))}
+                                {:time (int (:generation-count row)) :item "best fitness" :quantity (double (:best-fitness row))}))
+                (dyn/read-evolution-stats-from-table evolution-id))))
 
-(def line-plot
+(defn get-line-plot-map
+  "Data should be in a map with :time, :item, and :quantity fields"
+  [data]
   {:width 1400
    :height 800
    :data {:values data}
    :encoding {:x {:field "time" :type "quantitative"}
-              :y {:field "quantity" :type "quantitative" :scale {:domain [18000 40000]}}
+              :y {:field "quantity" :type "quantitative" :scale {:domain [18000 42000]}}
               :color {:field "item" :type "nominal"}}
    :mark "line"})
 
+(defn gather-statistics
+  "Gathers transaction-related statistics from the database."
+  [evolution-id]
+  (let [transactions (dyn/read-transactions-from-table evolution-id)]
+    (loop [transaction-count 0
+           long-transaction-count 0
+           short-transaction-count 0
+           profiting-long 0
+           profiting-short 0
+           total-profit 0
+           remaining-transactions transactions]
+      (log/info "transaction count:" transaction-count)
+      (if (not-empty remaining-transactions)
+        (let [transaction (first remaining-transactions)
+              position (:position transaction)
+              result (:result transaction)
+              time-range (:time-range transaction)]
+          (if (= position "long")
+            (if (> result 0)
+              (recur (inc transaction-count)
+                     (inc long-transaction-count)
+                     short-transaction-count
+                     (inc profiting-long)
+                     profiting-short
+                     (+ total-profit result)
+                     (rest remaining-transactions))
+              (recur (inc transaction-count)
+                     (inc long-transaction-count)
+                     short-transaction-count
+                     profiting-long
+                     profiting-short
+                     (+ total-profit result)
+                     (rest remaining-transactions)))
+            (if (> result 0)
+              (recur (inc transaction-count)
+                     long-transaction-count
+                     (inc short-transaction-count)
+                     profiting-long
+                     (inc profiting-short)
+                     (+ total-profit result)
+                     (rest remaining-transactions))
+              (recur (inc transaction-count)
+                     long-transaction-count
+                     (inc short-transaction-count)
+                     profiting-long
+                     profiting-short
+                     (+ total-profit result)
+                     (rest remaining-transactions)))))
+        (do (log/info "Total transactions:" transaction-count)
+            (log/info "Long transactions:" long-transaction-count)
+            (log/info "Short transactions:" short-transaction-count)
+            (log/info "Profiting long transactions:" profiting-long)
+            (log/info "Profiting short transactions:" profiting-short)
+            (log/info "Total profit:" total-profit)
+            (dyn/write-evolution-final-stats-to-table evolution-id {:total-transaction-count transaction-count
+                                                                    :long-transaction-count long-transaction-count
+                                                                    :short-transaction-count short-transaction-count
+                                                                    :profiting-long profiting-long
+                                                                    :profiting-short profiting-short
+                                                                    :total-profit total-profit}))))))
+; TODO: yukaridaki statlari evolution-stats veya direk evolution tablosuna yazalim
+(gather-statistics "b04084c0-9b9f-4ff8-bf96-cbc6e3d4d75a")
+
+; TODO: bir strateji icin tum entry/exit point'leri al, sonrasinda bunlari grafik uzerinde isaretle, bununla birlikte de fiyatin grafigini ciz
+
 ;; Render the plot
-(oz/view! line-plot)
+(oz/view! (get-line-plot-map (get-evolution-stat-data "evolution id here")))
--- src/clojure_scraps/dynamo.clj
+++ src/clojure_scraps/dynamo.clj
@@ -3,7 +3,8 @@
             [clojure-scraps.params :as p]
             [clojure.spec.alpha :as s]
             [taoensso.faraday :as far]
-            [envvar.core :as envvar :refer [env]]))
+            [envvar.core :as envvar :refer [env]]
+            [clojure.tools.logging :as log]))
 
 (def faraday-client-opts-live {:access-key (:aws-access-key @env)
                                :secret-key (:aws-secret-key @env)
@@ -11,8 +12,8 @@
 (def faraday-client-opts-local {:endpoint "http://localhost:8000"})
 (def faraday-client-opts faraday-client-opts-local)
 
-(def strategy-table-vars {:table-name :strategy-v1 :table-key :id})
-(def transaction-table-vars {:table-name :transaction-v1 :table-key :id})
+(def strategy-table-vars {:table-name :strategy-v1 :table-key :id :index "evolution-id-index"})
+(def transaction-table-vars {:table-name :transaction-v2 :table-key :id :index "strategy-id-index"})
 (def evolution-table-vars {:table-name :evolution-v1 :table-key :id})
 (def evolution-stats-table-vars {:table-name :evolution-stats-v1 :table-key :evolution-id})
 
@@ -38,14 +39,42 @@
 (defn write-transaction-to-table
   "Records the given transaction to database"
   [strategy-id transaction]
-  (far/put-item faraday-client-opts
-                (:table-name transaction-table-vars)
-                {:id (str (uuid/v4))
-                 :strategy-id strategy-id
-                 :result (:result transaction)
-                 :position (:position transaction)
-                 :time-range (:time-range transaction)}))
+  (log/info "writing transaction to table")
+  (time (far/put-item faraday-client-opts
+                      (:table-name transaction-table-vars)
+                      {:id (str (uuid/v4))
+                       :strategy-id strategy-id
+                       :result (:result transaction)
+                       :position (:position transaction)
+                       :time-range (:time-range transaction)})))
 
+(defn read-transactions-from-table
+  "Scans the transaction-v1 table for the evolution with the given id"
+  [evolution-id]
+  (let [strategy-ids (map :id (far/query faraday-client-opts
+                                         (:table-name strategy-table-vars)
+                                         {:evolution-id [:eq evolution-id]}
+                                         {:index (:index strategy-table-vars)}))]
+    (flatten (map (fn [strategy-id] (far/query faraday-client-opts
+                                               (:table-name transaction-table-vars)
+                                               {:strategy-id [:eq strategy-id]}
+                                               {:index (:index transaction-table-vars)})) strategy-ids))))
+
+;(read-transactions-from-table  "b04084c0-9b9f-4ff8-bf96-cbc6e3d4d75a")
+;(far/describe-table faraday-client-opts :transaction-v2)
+
+(defn migrate-transactions
+  []
+  (loop [i 0]
+    (if (< i 500)
+      (let [items (far/scan faraday-client-opts
+                            (:table-name transaction-table-vars)
+                            {:limit 1000})]
+        (doall (map (fn [item] (far/put-item faraday-client-opts :transaction-v2 item)
+                      (far/delete-item faraday-client-opts :transaction-v1 {:id (:id item)})) items))
+        (println "i:" i)
+        (recur (inc i))))))
+;(migrate-transactions)
 (defn write-evolution-to-table
   "Records the evolution parameters to database with given id"
   [evolution-id]
@@ -55,6 +84,17 @@
                        :id evolution-id
                        :timestamp (java.lang.System/currentTimeMillis))))
 
+(defn write-evolution-final-stats-to-table
+  "Records the final statistics of the evolution"
+  [evolution-id stats-map]
+  (far/update-item faraday-client-opts
+                   (:table-name evolution-table-vars)
+                   {:id evolution-id}
+                   {:update-expr "SET #name = :value"
+                    :expr-attr-names {"#name" "final-stats"}
+                    :expr-attr-vals {":value" (far/freeze stats-map)}
+                    :return :all-new}))
+
 (defn read-evolution-from-table
   "Queries the evolution-v1 table for the evolution with the given id"
   [evolution-id]
@@ -62,6 +102,7 @@
                 (:table-name evolution-table-vars)
                 {(:table-key evolution-table-vars) evolution-id}))
 
+;(read-evolution-from-table   "b04084c0-9b9f-4ff8-bf96-cbc6e3d4d75a")
 (defn write-evolution-stats-to-table
   [evolution-id generation-count best-fitness avg-fitness]
   (far/put-item faraday-client-opts
