d195b2f4f04b92c367f0c6a1cb27dddd64d0705a
Berke Tezergil <btezergil@gmail.com>
2025-01-12 21:10:02 +0300

    Added regression

--- deeplearning/lstm.py
+++ deeplearning/lstm.py
@@ -3,29 +3,37 @@ import numpy as np
 import tensorflow as tf
 
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 from tensorflow.keras.models import Sequential
 
-training_data = pd.read_csv("../eurusd-3month-1h-DL.csv")
-test_data = pd.read_csv("../eurusd-45days-1h-test-DL.csv")
+# implementation from: https://github.com/034adarsh/Stock-Price-Prediction-Using-LSTM/blob/main/LSTM_Improved_model(diff_dataset).ipynb
 
-scaler = MinMaxScaler(feature_range=(0, 1))
-data_training_array = scaler.fit_transform(training_data)
-data_test_array = scaler.fit_transform(test_data)
+training_data = pd.read_csv("../eurusd-3month-1h-DL.csv", sep=";")
+test_data = pd.read_csv("../eurusd-45days-1h-test-DL.csv", sep=";")
 
-x_train = []
-y_train = []
-for i in range(100, data_training_array.shape[0]):
-    x_train.append(data_training_array[i - 100 : i])
-    y_train.append(data_training_array[i, 0])
-x_train, y_train = np.array(x_train), np.array(y_train)
 
-x_test = []
-y_test = []
-for i in range(100, data_test_array.shape[0]):
-    x_test.append(data_test_array[i - 100 : i])
-    y_test.append(data_test_array[i, 0])
-x_test, y_test = np.array(x_test), np.array(y_test)
+def dl_data_prep():
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    data_training_array = scaler.fit_transform(training_data)
+    data_test_array = scaler.fit_transform(test_data)
+
+    x_train = []
+    y_train = []
+    for i in range(100, data_training_array.shape[0]):
+        x_train.append(data_training_array[i - 100 : i])
+        y_train.append(data_training_array[i, 0])
+    x_train, y_train = np.array(x_train), np.array(y_train)
+
+    x_test = []
+    y_test = []
+    for i in range(100, data_test_array.shape[0]):
+        x_test.append(data_test_array[i - 100 : i])
+        y_test.append(data_test_array[i, 0])
+    x_test, y_test = np.array(x_test), np.array(y_test)
+    y_test = scaler.inverse_transform(y_test.reshape(-1, 1))
+    return x_train, y_train, x_test, y_test
 
 
 def new_model(x_train):
@@ -68,11 +76,24 @@ def train_model(model):
         model.save("keras_model.h5")
 
 
-model = load_existing_model()
-y_pred = model.predict(x_test)
+def predict_using_lstm(x_test, model):
+    model = load_existing_model()
+    y_pred = model.predict(x_test)
+    y_pred = scaler.inverse_transform(y_pred)
+    return y_pred
 
-y_pred = scaler.inverse_transform(y_pred)
-y_test = scaler.inverse_transform(y_test.reshape(-1, 1))
+
+def regression():
+    x_train = training_data.drop(["close"], axis=1)
+    y_train = training_data.drop(["open", "low", "high"], axis=1)
+    x_test = test_data.drop(["close"], axis=1)
+    y_test = test_data.drop(["open", "low", "high"], axis=1)
+
+    regressor = LinearRegression()
+    regressor.fit(x_train, y_train)
+    y_pred = regressor.predict(x_test)
+    print(regressor.score(x_test, y_test))
+    return y_pred, y_test
 
 
 def calculate_total_profit(y_pred, y_test):
@@ -116,4 +137,6 @@ def calculate_total_profit(y_pred, y_test):
     return sum(transactions) - commission * len(transactions)
 
 
+y_pred, y_test = regression()
+y_pred, y_test = np.array(y_pred), np.array(y_test)
 total_profit = calculate_total_profit(y_pred, y_test)
