093a4b54f246f6528f5d8f95f1467773b4dbca24
Berke Tezergil <btezergil@gmail.com>
2025-03-03 11:31:18 +0300

    CodeBuild changes

--- aws-infra/stop-service.sh
+++ aws-infra/stop-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
-[ -d "/home/ec2-user/app/aws-infra/release" ] && \
-cd /home/ec2-user/app/aws-infra/release && \
+[ -d "/home/ec2-user/app/release" ] && \
+cd /home/ec2-user/app/release && \
 npm stop
