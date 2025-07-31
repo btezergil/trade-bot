aa2f5ea141bff3af46095c01aacda7664389ff79
Berke Tezergil <btezergil@gmail.com>
2025-07-31 21:24:49 +0300

    Changed experiment params

--- src/clojure_scraps/results.clj
+++ src/clojure_scraps/results.clj
@@ -1,17 +1,29 @@
 (ns clojure-scraps.results)
 
-(def hybrid-100gen-100pop-3height-ids
+(def hybrid-100pop-100gen-3height-ids
   ["b66e29b7-ac67-4673-8cfd-7cda0003745c"
    "a971d920-f0bc-45dc-b988-af01b8a4055d"
    "34b37f95-3a69-4b21-98a0-5703c2ecb844"
    "22ccc0f6-df44-4515-a146-78a6155f7307"
    "74d0d1ee-3fb4-4ca0-94a8-467a5e866c34"])
 
-(def hybrid-100gen-100pop-4height-ids
+(def hybrid-100pop-100gen-4height-ids
   ["5bbe089f-65ce-4694-8380-bca1926ab674"
    "873ac8db-295e-422c-a000-5a5aae02a4e2"
    "3d185188-a029-41d5-b0f4-03c23bc81861"
    "d33d779a-fdf7-4483-895b-df7d99963e4c"
    "e6bdc048-3d11-40c6-a5db-c680d780f9e5"])
 
+(def hybrid-100pop-200gen-3height-ids
+  ["5bbe089f-65ce-4694-8380-bca1926ab674"
+   "873ac8db-295e-422c-a000-5a5aae02a4e2"
+   "3d185188-a029-41d5-b0f4-03c23bc81861"
+   "d33d779a-fdf7-4483-895b-df7d99963e4c"
+   "e6bdc048-3d11-40c6-a5db-c680d780f9e5"])
 
+(def hybrid-100pop-200gen-4height-ids
+  ["0b8cb1dc-7e5d-4e21-a9f2-8f4c4867feb8"
+   "2431390f-ffe0-42b6-a0b4-8eabd5fd2fc3"
+   "70bff706-a2c8-41b8-8f13-588a22b8afeb"
+   "8483294c-dfc4-4a2a-9c2f-77b3c822e8c7"
+   "d4f0986c-1d16-44be-bd7b-9eab79f869e9"])
