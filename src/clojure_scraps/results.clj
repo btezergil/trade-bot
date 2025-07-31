652aa4b6df0037054d92a5173116e2106ac88b5b
Berke Tezergil <btezergil@gmail.com>
2025-07-31 21:24:49 +0300

    Changed experiment params

--- src/clojure_scraps/results.clj
+++ src/clojure_scraps/results.clj
@@ -1,17 +1,123 @@
 (ns clojure-scraps.results)
 
-(def hybrid-100gen-100pop-3height-ids
+(def hybrid-100pop-100gen-3height-ids
   ["b66e29b7-ac67-4673-8cfd-7cda0003745c"
    "a971d920-f0bc-45dc-b988-af01b8a4055d"
    "34b37f95-3a69-4b21-98a0-5703c2ecb844"
    "22ccc0f6-df44-4515-a146-78a6155f7307"
    "74d0d1ee-3fb4-4ca0-94a8-467a5e866c34"])
 
-(def hybrid-100gen-100pop-4height-ids
+(def hybrid-100pop-100gen-4height-ids
   ["5bbe089f-65ce-4694-8380-bca1926ab674"
    "873ac8db-295e-422c-a000-5a5aae02a4e2"
    "3d185188-a029-41d5-b0f4-03c23bc81861"
    "d33d779a-fdf7-4483-895b-df7d99963e4c"
    "e6bdc048-3d11-40c6-a5db-c680d780f9e5"])
 
+(def hybrid-100pop-200gen-3height-ids
+  ["5bbe089f-65ce-4694-8380-bca1926ab674"
+   "873ac8db-295e-422c-a000-5a5aae02a4e2"
+   "3d185188-a029-41d5-b0f4-03c23bc81861"
+   "d33d779a-fdf7-4483-895b-df7d99963e4c"
+   "e6bdc048-3d11-40c6-a5db-c680d780f9e5"])
 
+(def hybrid-100pop-200gen-4height-ids
+  ["0b8cb1dc-7e5d-4e21-a9f2-8f4c4867feb8"
+   "2431390f-ffe0-42b6-a0b4-8eabd5fd2fc3"
+   "70bff706-a2c8-41b8-8f13-588a22b8afeb"
+   "8483294c-dfc4-4a2a-9c2f-77b3c822e8c7"
+   "d4f0986c-1d16-44be-bd7b-9eab79f869e9"])
+
+(def hybrid-100pop-300gen-3height-ids
+  ["10df62b5-835e-4dcb-af50-b354df132609"
+   "3c159341-b14f-49f6-b194-0e0fc6b85ed6"
+   "40b0d497-09ee-43d1-8ff5-6c58601d5b56"
+   "434e325a-2b88-493b-bb00-8a48c3be3849"
+   "f99dc2ea-b237-41ea-8e1c-900971635b2a"])
+
+(def hybrid-100pop-300gen-4height-ids
+  ["05f21b38-bc13-4ed7-8adc-4f1dc32e0fe7"
+   "15ff8586-ed3c-40a6-af32-5155c9507383"
+   "34fcf052-5206-4221-af9e-69292c9118e6"
+   "482744cc-740b-4018-8379-7c68bcad6fd3"
+   "9c1983a5-7bea-4eea-a7d8-6eb46728a4be"])
+
+; 2519486-90
+(def hybrid-100pop-400gen-3height-ids
+  ["30d20e2d-e049-4447-89a9-e931a10716f1"
+   "331aa1f9-8923-4b47-993f-630bfb735c4e"
+   "440d78ca-d9a2-46b5-bfb7-37b0c7043314"
+   "90e265e7-fd03-44ed-aac7-606865bc6ce5"
+   "b227a981-f24a-4de4-96d8-c0fa05fe161a"])
+
+; 2519499-503
+(def hybrid-100pop-400gen-4height-ids
+  ["2d488997-a389-4635-88c7-fb7db360f297"
+   "55384035-fb86-4f1f-8664-5d854e4de589"
+   "750c9381-f791-4dbb-9984-91099c494a3c"
+   "819c3716-f42c-4d22-b93d-60279b5cb700"
+   "f4ba37a8-5158-40c4-a896-557c5352abe1"])
+
+; 2521240-4
+(def accuracy-perc-100pop-100gen-3height-ids
+  ["6120a3a1-c5a7-4452-8b0a-cc2697e92907"
+   "6e8594d7-e717-454c-9a2a-473cffd3a8b0"
+   "8c7ec638-e4f6-4ca2-a2a3-f186e45246ca"
+   "9e882786-daeb-4918-bda9-046a91b6605c"
+   "e57e639c-4e56-444d-822b-5799ba8b3c5a"])
+
+; 2521257-61
+(def accuracy-perc-100pop-100gen-4height-ids
+  ["0f1af622-167a-4b2e-871e-f105eb411dbf"
+   "1dd623c5-a2d1-4853-b8fc-872bb078e407"
+   "31bfdebc-b3e9-453d-b8c4-7ba0acff4115"
+   "607dd4d5-f3d3-4413-8e31-fc5eb3646946"
+   "bbf222ef-9b38-43e8-b182-1d7c96d96e7a"])
+
+; 2521301-5, 25236046
+(def accuracy-perc-100pop-200gen-3height-ids
+  ["028471b1-36b8-400b-bbf9-82cd236eee57"
+   "3c27e8ef-0c98-48f3-ab23-9f01b26992be"
+   "66338e8f-3e87-45af-8402-8c691243ad68"
+   "c1ae022c-d6ab-4456-bc3b-b5f4cb2ff330"
+   "5c2f999d-37f7-433a-adb1-0708f8a6d176"])
+
+; 2521348-55
+(def accuracy-perc-100pop-200gen-4height-ids
+  ["89a741f9-d722-4f6b-979c-850327a3545f"
+   "8bb7e262-1264-44c2-9ea7-d5eb38f93063"
+   "8bdc8b00-d584-49aa-b069-0cc15c719756"
+   "dc4f944b-3ed8-4b5c-9c1e-32dc9ad02182"
+   "eff43ad0-0945-43ae-9690-3fe67c89e45b"])
+
+; 2523395-9
+(def accuracy-perc-100pop-300gen-3height-ids
+  ["6fa6b23d-e3bb-41d2-a42e-4c2cfa1d7a97"
+   "7dc9426d-be76-4e45-af12-008718c6f184"
+   "d652407f-bba1-4a15-8727-505bf29d1bc7"
+   "e851d2b4-1fa5-4c99-9d7c-ac76b2829f6d"
+   "f0fb2fa9-1bb4-4c05-ac87-e45f7476b8c2"])
+
+; 2523557-61
+(def accuracy-perc-100pop-300gen-4height-ids
+  ["4d24f5cc-201b-4d5c-8021-0009edc05b9a"
+   "53edbaab-ccf2-447e-a4d4-4c246a3e0dc7"
+   "5b670d37-1d1b-4ca2-a570-335492c7f964"
+   "9fde72a7-e069-452d-9c72-2d101c58f7c8"
+   "a26b97a9-d5d6-4b18-9d7d-d333db96fdaa"])
+
+; 2529434-9
+(def accuracy-perc-100pop-400gen-3height-ids
+  ["b66e29b7-ac67-4673-8cfd-7cda0003745c"
+   "a971d920-f0bc-45dc-b988-af01b8a4055d"
+   "34b37f95-3a69-4b21-98a0-5703c2ecb844"
+   "22ccc0f6-df44-4515-a146-78a6155f7307"
+   "74d0d1ee-3fb4-4ca0-94a8-467a5e866c34"])
+
+; 2521348-55  2530732 ->  2 parallel
+(def accuracy-perc-100pop-400gen-4height-ids
+  ["5bbe089f-65ce-4694-8380-bca1926ab674"
+   "873ac8db-295e-422c-a000-5a5aae02a4e2"
+   "3d185188-a029-41d5-b0f4-03c23bc81861"
+   "d33d779a-fdf7-4483-895b-df7d99963e4c"
+   "e6bdc048-3d11-40c6-a5db-c680d780f9e5"])
